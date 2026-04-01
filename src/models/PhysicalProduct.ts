ed4e4133be755d7318f4a980b1a058e712b3e792
Taron Bentley <taronbentley1@gmail.com>
2026-04-01 18:11:28 -0400

    filled in most of the different product sections

---
+++ src/models/PhysicalProduct.ts
@@ -0,0 +1,17 @@
+import { Product } from './Product';
+
+export class PhysicalProduct extends Product {
+    weight: number;
+
+    constructor(sku: string, name: string, price: number, weight: number) {
+        super(sku, name, price);
+        this.weight = weight;
+    }
+
+    override getPriceWithTax(): number {
+        return this.price * 1.1;
+    }
+
+    get formattedWeight(): string {
+        return `${this.weight} kg`;
+    }
