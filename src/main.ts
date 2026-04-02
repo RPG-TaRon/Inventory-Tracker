ba5b6f09bf81f79f2a12518698e790079dbcd352
Taron Bentley <taronbentley1@gmail.com>
2026-04-02 10:04:34 -0400

    added info in tax files and started main file

---
+++ src/main.ts
@@ -0,0 +1,4 @@
+import { PhysicalProduct } from "./models/PhysicalProduct.js";
+import { DigitalProduct } from "./models/DigitalProduct.js";
+import { Product } from "./models/Product.js";
+import { calculateTax } from "./utils/taxCalculator.js";
